4c843296dc37ee8a9dd90a844b8a229bf93ff250
Nathan94600 <nathan94nahtan@gmail.com>
2026-01-22 19:06:40 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Mon site React</h1>
+      <p>Publié sur Vercel avec déploiement automatique</p>
+    </div>
+  );
+}
+
+export default App;
